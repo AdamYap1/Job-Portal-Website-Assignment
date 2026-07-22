3c7a3b29a2c29e297954b75050f388b57a29e69c
Adam Yap <181008216+AdamYap1@users.noreply.github.com>
2026-07-23 02:31:12 +0800

    Models changes, and database tables creation

---
+++ Models/JobSeeker.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Job_Portal_Website.Models
+{
+    public class JobSeeker
+    {
+        [Key]
+        public int jobSeekerId { get; set; }
+        public string seekerName { get; set; }
+        public string seekerEmail { get; set; }
+        public string seekerPassword { get; set; }
+        public string?seekerSkills { get; set; }
+        public string? seekerExp {  get; set; }
+        public string? resumePath { get; set; }
+        public ICollection<Application> Applications { get; set; } = new List<Application>();
+
+    }
+}
