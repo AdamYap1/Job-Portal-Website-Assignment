999d50e208915468b7b31f6e40f04bade133d31f
Adam Yap <181008216+AdamYap1@users.noreply.github.com>
2026-08-06 04:29:21 +0800

    US08 complete

--- obj/Debug/net10.0/JobPortalWebsite.AssemblyInfo.cs
+++ obj/Debug/net10.0/JobPortalWebsite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobPortalWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cda904a9df6e8a38a32aad6a9fcd0cfa33cada99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63bcd92caef5c96d47f3958f72b8acfc6d5dbfd2")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobPortalWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobPortalWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
