2f989b9c34330019b25827db35a4c48eae56f988
Jesse Chan <jingxingchan@gmail.com>
2026-08-06 11:16:08 +0800

    save

--- obj/Debug/net10.0/JobPortalWebsite.AssemblyInfo.cs
+++ obj/Debug/net10.0/JobPortalWebsite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobPortalWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba873455d216b027764db2a56d8b7236a1583d56")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12bcbf7a93c4895e6bfdb9dcf159866acf4e8a44")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobPortalWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobPortalWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
