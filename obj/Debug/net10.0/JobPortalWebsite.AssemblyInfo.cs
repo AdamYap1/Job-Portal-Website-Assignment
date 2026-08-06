dce51efad63014f4792624f43b4884b5d4f6d80e
Jesse Chan <jingxingchan@gmail.com>
2026-08-06 11:26:47 +0800

    fix

--- obj/Debug/net10.0/JobPortalWebsite.AssemblyInfo.cs
+++ obj/Debug/net10.0/JobPortalWebsite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("JobPortalWebsite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f989b9c34330019b25827db35a4c48eae56f988")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b776a0c8707fb767d0795359815a73354222e95")]
 [assembly: System.Reflection.AssemblyProductAttribute("JobPortalWebsite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("JobPortalWebsite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
